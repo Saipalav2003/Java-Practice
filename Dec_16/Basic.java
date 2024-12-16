19e57880d923bc3489d9eed3e71703bc615c5f0a
Saipalav2003 <165296978+Saipalav2003@users.noreply.github.com>
2024-12-16 06:59:29 +0000

    Basic datatype

--- Dec_16/Basic.java
+++ Dec_16/Basic.java
@@ -1,7 +1,17 @@
 public class Basic{
     public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Int add "+ (a+b));
 
-        System.out.println("Hello word");
+        System.out.println("Int add "+ (a-b));
+
+        System.out.println("Int add "+ (a*b));
+
+        System.out.println("Int add "+ (a/b));
+
+
+
     }
 
 }
