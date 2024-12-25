9b07720acce78dc48d9340826bc4ec2dcda4a584
Saipalav2003 <165296978+Saipalav2003@users.noreply.github.com>
2024-12-25 03:59:56 +0000

    Nested loop in java

---
+++ Dec_24/loop.java
@@ -0,0 +1,13 @@
+package Dec_24;
+public class loop {
+    public static void main(String[] args){
+
+        for(int a=0; a<=5; a++){
+            
+            for(int i=0; i<a; i++){
+                System.out.print(i);
+            }
+            System.out.println(a)
+        }
+    }
+}
