b0f3259eb995c92c024dfa6c811d2d4e7d26bdf9
Saipalav2003 <165296978+Saipalav2003@users.noreply.github.com>
2025-01-03 04:06:41 +0000

    Functions in java

---
+++ Jan_3/Function/BasicFunction.java
@@ -0,0 +1,19 @@
+package Jan_3.Function;
+
+public class BasicFunction {
+    
+public static void info(){
+    System.out.println("This is a Basic function...");
+    System.out.println("My name is Sai Palav...");
+}
+
+
+public static void main(String[] args) {
+
+    info();
+    info();
+    
+    
+}
+
+}
