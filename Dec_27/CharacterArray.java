e933375ff6f8d3839e459267ce22fc13af5cfe82
Saipalav2003 <165296978+Saipalav2003@users.noreply.github.com>
2025-01-03 04:00:20 +0000

    Array string and character

---
+++ Dec_27/CharacterArray.java
@@ -0,0 +1,14 @@
+public class CharacterArray {
+    public static void main(String[] args) {
+        Char data[] = new Char[3];
+
+        data[0] = "s";
+        data[1] = "A";
+        data[2] = "I";
+
+        for (int = i; i < data.length; i++){
+            System.out.println((i+1)+" = "+data[i]);
+        }
+
+    }
+}
--- Dec_27/StringArray.java
+++ Dec_27/StringArray.java
@@ -1,3 +1,15 @@
 public class StringArray {
-    
+    public static void main(String[] args) {
+        String data[] = new String[5];
+
+        data[0] = "JAVA";
+        data[1] = "HTML";
+        data[2] = "CSS";
+        data[3] = "C++";
+        data[4] = "SQL";
+
+        for (int i = 0; i < data.length; i++){
+            System.out.println((i+1)+" = "+data[i]);
+        }
+    }
 }
