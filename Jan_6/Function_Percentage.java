316fc208c5ad9994bf0a6a267afb87cf264e4ae1
Saipalav2003 <165296978+Saipalav2003@users.noreply.github.com>
2025-01-08 03:58:43 +0000

    Grade in java

--- Jan_6/Function_Percentage.java
+++ Jan_6/Function_Percentage.java
@@ -2,6 +2,7 @@ package Jan_6;
 
 public class Function_Percentage {
     static int Total;
+    static int per;
     static void marks(int Java, int CPP, int SQL, int Python, int Javascript){
         int total = Java + CPP + SQL + Python + Javascript;
         Total = total;
@@ -17,10 +18,31 @@ public class Function_Percentage {
     static void percentage(){
         int percentage = (Total*100)/500;
         System.out.println("Percentage :" +percentage);
+        per = percentage;
+    }
+
+    static void grade(){
+        if(per < 100 && per > 80){
+            System.out.println("Grade A");
+        }else if(per < 80 && per > 60){
+            System.out.println("B Grade");
+        }else if(per < 60 && per > 40){
+            System.out.println("C Grade");
+        }else if(per < 40 && per > 20){
+            System.out.println("D Grade");
+        }else{
+            System.out.println("Fail");
+        }
+       
     }
 
     public static void main(String[] args) {
+        
         marks(67, 57, 79, 54, 66);
         percentage();
+        
+        grade();
+
+  
     }
 }
